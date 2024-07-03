70b8c5bc65aabeb6977de221db68663db5228aa5
sxd515 <31696288+sxd515@users.noreply.github.com>
2024-07-03 10:32:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,10 @@ ingredients_list= st.multiselect (
 )
 
 if ingredients_list:
-   #ingredients_string = ''
+   ingredients_string = ''
    #st.text(ingredients_list)
 
-  ingredients_string = ''
-    
+      
 for fruit_chosen in ingredients_list:
      ingredients_string += fruit_chosen + ' , '
         
