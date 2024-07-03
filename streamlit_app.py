af4c53795d44e5263a71ec55cf19c1954d34eb0f
sxd515 <31696288+sxd515@users.noreply.github.com>
2024-07-03 12:43:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,14 @@ st.dataframe(pd_df)
 st.stop()
 
 ingredients_list= st.multiselect (
-    'Choose upto 5 ingridients', my_dataframe , max_selections=5
-)
+    'Choose upto 5 ingridients', my_dataframe , max_selections=5)
 
 if ingredients_list: 
     ingredients_string = ''
    #st.text(ingredients_list)
  
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' , '
+        ingredients_string += fruit_chosen + ' '
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
