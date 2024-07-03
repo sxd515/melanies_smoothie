3061896d700313cfdf8cc1f638756a6ec66af436
sxd515 <31696288+sxd515@users.noreply.github.com>
2024-07-03 10:36:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ingredients_list:
 for fruit_chosen in ingredients_list:
      ingredients_string += fruit_chosen + ' , '
         
-     st.write(ingredients_string)
+    #st.write(ingredients_string)
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
          values ('""" + ingredients_string +  """' , '""" + name_on_order + """')"""
         
